85770c9aa573c939a43ad8d826a0b0912edd7f37
TyphaonTyphon <65467884+TyphaonTyphon@users.noreply.github.com>
2020-11-23 03:24:31 +0300

    Update InviteCommand.ts

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -5,16 +5,16 @@ import { createEmbed } from "../utils/createEmbed";
 import { disableInviteCmd } from "../config";
 
 @DefineCommand({
-    name: "invite",
-    description: "Get the bot's invite link",
-    usage: "{prefix}invite",
+    name: "davet",
+    description: "Botun davet bağlantısını alın",
+    usage: "{prefix}davet",
     disable: disableInviteCmd
 })
 export class InviteCommand extends BaseCommand {
     public async execute(message: IMessage): Promise<void> {
         message.channel.send(
             createEmbed("info")
-                .addField("Discord bot invite link", `[Click here](${await this.client.generateInvite({ permissions: 53857345 })})`)
+                .addField("Discord bot davet bağlantısı", `[Buraya Tıkla](${await this.client.generateInvite({ permissions: 53857345 })})`)
         ).catch(e => this.client.logger.error("PLAY_CMD_ERR:", e));
     }
 }
